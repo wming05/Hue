49b271c40efd784d19e6a11db6a8db2364b87808
Romain Rigaux <romain@cloudera.com>
2017-09-07 11:55:41 -0700

    HUE-7226 [frontend] Log when the editor operations hang

--- desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
+++ desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
@@ -1187,6 +1187,11 @@ var EditorViewModel = (function() {
 
     self.executingBlockingOperation = null; // A ExecuteStatement()
     self.showLongOperationWarning = ko.observable(false);
+    self.showLongOperationWarning.subscribe(function(newValue) {
+      if (newValue) {
+        hueAnalytics.convert('editor', 'showLongOperationWarning');
+      }
+    });
 
     var longOperationTimeout = -1;
 
